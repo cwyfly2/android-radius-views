fc334afc1e2960bd97a6d456b43e03f0624f486d
cwyfly2 <cwy.fly2@gmail.com>
2017-07-22 02:02:31 +0800

    init

---
+++ library/src/main/java/com/laughfly/radius/RadiusViewDelegate.java
@@ -0,0 +1,21 @@
+package com.laughfly.radius;
+
+import android.graphics.Paint;
+import android.graphics.drawable.GradientDrawable;
+
+/**
+ * Created by cwy on 2017/7/22.
+ */
+
+public class RadiusViewDelegate {
+    private int mRadius;
+
+    private int[] mRadii;
+
+    private GradientDrawable mGradientDrawable;
+
+    public RadiusViewDelegate() {
+        Paint paint;
+    }
+
+}
